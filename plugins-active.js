85b7064790194292729ac9b76a5fa226eb9b5668
AdrianCraft07 <proadrian684@gmail.com>
2023-03-23 18:43:37 -0600

    plugins / package.json -> scripts.init

--- plugins-active.js
+++ plugins-active.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
 const cp = require('child_process');
 fs.readdirSync(__dirname+'/plugins').forEach(file => {
-  cp.execSync(`cd ${__dirname}/plugins/${file} && npm i`);
+  cp.execSync(`cd ${__dirname}/plugins/${file} && npm run init`);
 })
